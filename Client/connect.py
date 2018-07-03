fa636147ea8a843b62cb6a27d7ecc34c3b34b56e
nikilp <nikilp@gmail.com>
2018-07-03 16:00:46 +0800

    fix inconsistent use of tabs and spaces in indentation

--- Client/connect.py
+++ Client/connect.py
@@ -11,21 +11,21 @@ username_ipaddress = ""
 # A function that checks if there is an existing ssh process running in the backgroun:
 def ssh_running():
     try:	
-	out = subprocess.check_output("pgrep -x ssh", shell=True)
-        print out
+        out = subprocess.check_output("pgrep -x ssh", shell=True)
+        print(out)
     except subprocess.CalledProcessError as e:
-        print "Running and activating ssh. Please wait for 1 minute..."
+        print("Running and activating ssh. Please wait for 1 minute...")
 	run_ssh()
 
 # A	function that runs the ssh reverse tunnel
 def run_ssh():
     try:
-	full_ssh_command= "ssh -i %s -N -R %s:localhost:22 %s" % (location_of_pem_file, port_open, username_ipaddress)
-	ssh_output = subprocess.check_output(full_ssh_command, shell=True)
-	if not ssh_output:
-	    print "Successful"
+        full_ssh_command= "ssh -i %s -N -R %s:localhost:22 %s" % (location_of_pem_file, port_open, username_ipaddress)
+        ssh_output = subprocess.check_output(full_ssh_command, shell=True)
+        if not ssh_output:
+            print("Successful")
     except subprocess.CalledProcessError as e:
-        print "Failed. Please check your config file."
+        print("Failed. Please check your config file.")
 
 #Main Command
 
